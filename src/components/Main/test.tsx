d8f8e8e3126fbe512106b63fbe8d36c012862d39
Gustavo Pedrosa <luiz.gustavo.pedrosa@gmail.com>
2023-02-05 13:31:08 -0300

    feat: create first style for Main component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,15 @@ describe('<Main/>', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /Hello World!/i })
+      screen.getByRole('heading', { name: /React Avançado!/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })

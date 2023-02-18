7f6f89a41b5144feadb37de78a87d59bf634975e
Gustavo Pedrosa <luiz.gustavo.pedrosa@gmail.com>
2023-02-18 19:06:12 -0300

    chore: remove some addons for addons essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,7 @@ module.exports = {
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-knobs'
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react",
   "core": {
